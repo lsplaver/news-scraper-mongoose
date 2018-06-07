bf7c48067f1813ad24d31188cc4a8b788540eb81
lsplaver <lsplaver@yahoo.com>
2018-06-07 16:32:49 -0400

    update with article model ready

---
+++ models/Article.js
@@ -0,0 +1,47 @@
+var mongoose = require("mongoose");
+
+var Schema = mongoose.Schema;
+
+// creates a custom article schema for use by the article constructor
+var ArticleSchema = new Schema({
+    // headline is the class name for the title of the article from scaped site, but value is plain text of <a> tag
+    headline: {
+        type: string,
+        required: true,
+        trrim: true
+    },
+
+    // blurb is the class name for the summary of the article from scraped site, but value is the plain text of the <div> tag with class='blurb'
+    blurb: {
+        type: string,
+        required: true
+    },
+
+    // link actually comes before headline on the main page of scraped site, as the href property
+    linkL: {
+        type: string,
+        required: true
+    },
+
+    // Author's byline and link to profile of the article from the scraped site, both from property and plain text of <a> tag
+    author: {
+        authName: {
+            type: string,
+            required: false
+        },
+        authLink: {
+            type: string,
+            required: false
+        }
+    },
+
+    // linking the note created by the constructor and schema in Note.js
+    note: {
+        type: Schema.Types.ObjectId,
+        ref: "Note"
+    }
+});
+
+var Article = mongoose.model("Article", ArticleSchema);
+
+module.exports = Article;
